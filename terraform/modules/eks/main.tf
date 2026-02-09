6a567aa54fa672d98a5df88947a7ad3551c0f9f5
Paul Adegoke <farmerpisco@gmail.com>
2026-02-09 21:30:04 +0100

    Update terraform files

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -108,63 +108,73 @@ resource "aws_iam_role_policy_attachment" "pb_eks_AmazonEC2ContainerRegistryRead
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# Data source to get the OIDC provider URL
-data "aws_iam_openid_connect_provider" "eks_oidc" {
-  url = aws_eks_cluster.pb_eks_cluster.identity[0].oidc[0].issuer
-}
+# Create OIDC provider for the EKS cluster
+# data "tls_certificate" "eks_oidc" {
+#   url = aws_eks_cluster.pb_eks_cluster.identity[0].oidc[0].issuer
+# }
+
+# resource "aws_iam_openid_connect_provider" "eks_oidc" {
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = [data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint]
+#   url             = aws_eks_cluster.pb_eks_cluster.identity[0].oidc[0].issuer
+
+#   tags = {
+#     Name = "${var.project_name}-eks-oidc-provider"
+#   }
+# }
 
 # Extract OIDC provider ID from the URL
-locals {
-  oidc_provider_arn = data.aws_iam_openid_connect_provider.eks_oidc.arn
-  oidc_provider_id  = element(split("/", data.aws_iam_openid_connect_provider.eks_oidc.url), length(split("/", data.aws_iam_openid_connect_provider.eks_oidc.url)) - 1)
-}
+# locals {
+#   oidc_provider_arn = data.aws_iam_openid_connect_provider.eks_oidc.arn
+#   oidc_provider_id  = element(split("/", data.aws_iam_openid_connect_provider.eks_oidc.url), length(split("/", data.aws_iam_openid_connect_provider.eks_oidc.url)) - 1)
+# }
 
 # IAM role for EBS CSI Driver
-resource "aws_iam_role" "ebs_csi_driver_role" {
-  name = "AmazonEKS_EBS_CSI_DriverRole"
+# resource "aws_iam_role" "ebs_csi_driver_role" {
+#   name = "AmazonEKS_EBS_CSI_DriverRole"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = local.oidc_provider_arn
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "${replace(aws_eks_cluster.pb_eks_cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
-            "${replace(aws_eks_cluster.pb_eks_cluster.identity[0].oidc[0].issuer, "https://", "")}:aud" = "sts.amazonaws.com"
-          }
-        }
-      }
-    ]
-  })
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = {
+#           Federated = local.oidc_provider_arn
+#         }
+#         Action = "sts:AssumeRoleWithWebIdentity"
+#         Condition = {
+#           StringEquals = {
+#             "${replace(aws_eks_cluster.pb_eks_cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+#             "${replace(aws_eks_cluster.pb_eks_cluster.identity[0].oidc[0].issuer, "https://", "")}:aud" = "sts.amazonaws.com"
+#           }
+#         }
+#       }
+#     ]
+#   })
 
-  tags = {
-    Name = "${var.project_name}-ebs-csi-driver-role"
-  }
-}
+#   tags = {
+#     Name = "${var.project_name}-ebs-csi-driver-role"
+#   }
+# }
 
 # Attach AWS managed policy for EBS CSI Driver
-resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
-  role       = aws_iam_role.ebs_csi_driver_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
+# resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
+#   role       = aws_iam_role.ebs_csi_driver_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+# }
 
 # EBS CSI Driver Addon
-resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name             = aws_eks_cluster.pb_eks_cluster.name
-  addon_name               = "aws-ebs-csi-driver"
-  service_account_role_arn = aws_iam_role.ebs_csi_driver_role.arn
+# resource "aws_eks_addon" "ebs_csi_driver" {
+#   cluster_name             = aws_eks_cluster.pb_eks_cluster.name
+#   addon_name               = "aws-ebs-csi-driver"
+#   service_account_role_arn = aws_iam_role.ebs_csi_driver_role.arn
 
-  depends_on = [
-    aws_eks_node_group.pb_eks_node_group,
-    aws_iam_role_policy_attachment.ebs_csi_driver_policy
-  ]
+#   depends_on = [
+#     aws_eks_node_group.pb_eks_node_group,
+#     aws_iam_role_policy_attachment.ebs_csi_driver_policy
+#   ]
 
-  tags = {
-    Name = "${var.project_name}-ebs-csi-driver"
-  }
-}
+#   tags = {
+#     Name = "${var.project_name}-ebs-csi-driver"
+#   }
+# }
